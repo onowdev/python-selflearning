b435d3ce474879541e3a20134761ff43e14464d5
onowdev <onowdev@gmail.com>
2020-06-15 22:13:48 +0700

    operator aritmatika

---
+++ 6_python_aritmatika.py
@@ -0,0 +1,6 @@
+"""
+Operators are the constructs, which can manipulate 
+the value of operands. Consider the expression 
+4 + 5 = 9. Here, 4 and 5 are called 
+the operands and + is called the operator.
+"""
