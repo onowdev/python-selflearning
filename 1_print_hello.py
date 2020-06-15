45ef0d13f0248251940c58544cfe790f7fa57a01
onowdev <onowdev@gmail.com>
2020-06-15 15:07:27 +0700

    menambahkan materi variabel di string

--- 1_print_hello.py
+++ 1_print_hello.py
@@ -2,3 +2,5 @@ print("Hello World")
 print('Hello boys')
 a = input()
 print("Nama Kamu", a)
+
+print("Terima KAsih")
---
+++ 2_variabel.py
@@ -0,0 +1,19 @@
+counter = 100 # memasukan nilai yang bertipe integer
+miles = 890.0 # Tipe floating point
+name = "Andra" # Tipe String
+
+print(counter)
+print(miles)
+print(name)
+
+#python Strings
+print("=" * 50)
+str = "Hello World"
+
+print(str) # Mencetak Variabel yang ada di str dan bertipe "String"
+print(str[0]) #Mencetak karakter pertama dari variabel str huruf "H"
+print(str[2:5]) # Mencetak karakter string dari index ke 3 sampai index ke 5 , dan dimulai dari index 0
+print(str[2:]) # mencetak string Dimulai dari index ke 3
+print(str * 2) # mencetak String 2x
+print(str + "TEST") # mencetak string dengan menambahkan Kata TEST
+
