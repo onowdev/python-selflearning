755bf3038c7e755d487f95c901d5406057e4da24
onowdev <onowdev@gmail.com>
2020-06-16 09:20:07 +0700

    menambahkan materi aritmatika

--- 6_python_aritmatika.py
+++ 6_python_aritmatika.py
@@ -4,3 +4,20 @@ the value of operands. Consider the expression
 4 + 5 = 9. Here, 4 and 5 are called 
 the operands and + is called the operator.
 """
+a = 9
+b = 8
+
+c = a + b # Addition
+d = a - b # subtraction
+e = a * b # multiplication
+f = a % b # modulus
+g = a / b # floor Division
+h = a ** b # Exponent
+
+
+print ("Hasil dari penjumlahan adalah", c)
+print ("Hasil dari pengurangan adalah", d)
+print ("Hasil dari perkaliannya adalah", e)
+print ("Hasil dari sisa hasil bagi adalah", f)
+print ("Hasil dari pembagian nya adalah", g)
+print ("Hasil dari Pemangkatannya adalah", h)
