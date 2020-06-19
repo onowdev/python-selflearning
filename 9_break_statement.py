2ea9e5432d019753dfc32aed0d93010ced8a8709
onowdev <onowdev@gmail.com>
2020-06-19 14:41:34 +0700

    break Statement py

---
+++ 9_break_statement.py
@@ -0,0 +1,14 @@
+for letter in 'python': # Contoh Pertama
+    if letter == 'h':
+        break
+print('Current Letter:', letter)
+
+var = 10        #Secon Example
+
+while var > 0:
+    print('Current variable value:', var)
+    var = var -1
+    if var == 5:
+        break
+
+print("Good Bye!")
