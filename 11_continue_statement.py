d9409fe276df4c16bfa23b4a6c7ced25d17a3555
onowdev <onowdev@gmail.com>
2020-06-19 15:40:30 +0700

    continue Ststement

---
+++ 11_continue_statement.py
@@ -0,0 +1,12 @@
+for letter in 'python': #first Example
+    if letter == 'h':
+        continue
+    print('Current Letter :', letter)
+
+var = 10
+while var > 0:
+    var = var -1
+    if var == 5:
+        continue
+    print('Current Variable value :', var)
+print("Good Bye!")
