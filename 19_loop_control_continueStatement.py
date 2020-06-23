18b1fb058ba7a40477a92dc84216354cc9f4f254
onowdev <onowdev@gmail.com>
2020-06-23 09:53:05 +0700

    LoopControlContinueStatement

---
+++ 19_loop_control_continueStatement.py
@@ -0,0 +1,12 @@
+for letter in 'Python': # Fist exsample
+    if letter == 'o':
+        continue
+    print("Current Letter:", letter)
+
+var = 10
+while var > 0:
+    var = var -1
+    if var == 5:
+        continue
+    print("Current Variable value", var)
+print("Good Job")
