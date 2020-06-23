e4e99f7e1daf67fe67f0930bb17527dd9fd6c440
onowdev <onowdev@gmail.com>
2020-06-23 09:21:30 +0700

    Single Statement Suites

---
+++ 16_single_statement_suites.py
@@ -0,0 +1,7 @@
+flag = 1
+while (flag): print("Given Flag is really true!")
+print ("goodbye!")
+
+# It is better not try above example because it goes into 
+# infinite loop 
+# and you need to press CTRL+C keys to exit.
