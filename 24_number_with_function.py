247ec61cb5facfd4c91acb7524d1078aa0a9c805
onowdev <onowdev@gmail.com>
2020-06-24 12:42:55 +0700

    number with function

---
+++ 24_number_with_function.py
@@ -0,0 +1,8 @@
+import math # call the function
+
+print(math.sqrt(9)) #Sqrt Function And Return Value
+print(math.sin(30)) #Sin Function And Return Value
+
+
+# for more information visit 
+# https://docs.python.org/3/library/math.html
