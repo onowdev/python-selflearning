1ec7183c1aa498bdc83d326c69a5cb4a70aed2e4
onowdev <onowdev@gmail.com>
2020-06-25 09:54:30 +0700

    creating list

---
+++ 26_list.py
@@ -0,0 +1,17 @@
+'''
+The list is the most versatile datatype available in Python, 
+which can be written as a list of comma-separated values (items) 
+between square brackets. Important thing about a list is that the 
+items in a list need not be of the same type.
+Creating a list is as simple as putting different 
+comma-separated values between square brackets.
+'''
+# Create List
+
+list1 = ['pysics', 'chemistry', 1997, 2000];
+list2 = [1,2,3,4,5];
+list3 = ["a", "b", "c", "d",];
+
+print(list1)
+print(list2)
+print(list3)
