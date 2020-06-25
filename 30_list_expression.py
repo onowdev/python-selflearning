46d3b959041c4efd0b90a412271d8197a0b8214f
onowdev <onowdev@gmail.com>
2020-06-25 10:46:07 +0700

    list expression

---
+++ 30_list_expression.py
@@ -0,0 +1,18 @@
+list1 = [1,2,3,4,5,6,7]
+
+print(len(list1))  # length in list
+
+list2 = [8,9,10]
+
+print(list1+list2)  # Concatenation
+
+list3 = ['hi'] # Repetition
+
+print(list3 * 5)
+
+list4 = [1,2,3,4,5]
+
+print(3 in list4) # Membership
+
+for x in list4:
+    print(x,end='')
