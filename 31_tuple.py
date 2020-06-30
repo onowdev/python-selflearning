1f4f5d156616a866bb30b26cde96a87d44dd7cc9
onowdev <onowdev@gmail.com>
2020-06-30 10:35:20 +0700

    tuple

---
+++ 31_tuple.py
@@ -0,0 +1,12 @@
+# A tuple is a collection of objects which ordered and immutable. 
+# Tuples are sequences, just like lists. The main difference between 
+# the tuples and the lists is that the tuples cannot be changed unlike lists. 
+# Tuples use parentheses, whereas lists use square brackets.
+# Creating a tuple is as simple as putting different comma-separated values
+
+tup1 = ('physics', 'chemistry', 1997, 2000)
+tup2 = (1, 2, 3, 4, 5 )
+tup3 = "a", "b", "c", "d"
+
+print("tup1[0]:", tup1[0])
+print("tup2[1:5]:", tup2[1:5])
