12413f0c352d069d0c6f6b24ef404d7e5d5e973c
onowdev <onowdev@gmail.com>
2020-06-30 10:41:25 +0700

    dictionary

---
+++ 34_dictionary.py
@@ -0,0 +1,11 @@
+# Each key is separated from its value by a colon (:), 
+# the items are separated by commas, and the whole 
+# thing is enclosed in curly braces. 
+# An empty dictionary without any items is 
+# written with just two curly braces, like this: {}.
+
+
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+
+print ("dict['Name']: ", dict['Name'])
+print ("dict['Age']: ", dict['Age'])
