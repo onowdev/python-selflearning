ec78976bf544c52d9c2c2dd679c4672c00848cf8
onowdev <onowdev@gmail.com>
2020-08-18 21:50:06 +0700

    programingwithmosh_input type

---
+++ OOP-python/programing-with-mosh/input_var.py
@@ -0,0 +1,5 @@
+name = input("What's your name? ")
+print("Hi.. " + name)
+
+color = input("What's your favorite color? ")
+print(name + " favorite color is " + color)
---
+++ OOP-python/programing-with-mosh/tebakumur.py
@@ -0,0 +1,6 @@
+birth_year = input(" What's your birth year? ")
+
+age = 2020 - int(birth_year)
+
+print(" your age is "+ str(age))
+print(type(age))
